ed8a71c8806657f651f93611a1796604f7c7d86b
Jacob Bennett <kf7ags@gmail.com>
2018-10-05 08:57:20 -0600

    made so you can check off tasks

--- main.js
+++ main.js
@@ -3,15 +3,27 @@ let myUserInput = document.getElementById("userInput");
 
 let theList = [];
 
+function toMakeGreen(element){
+    let listItems = myTaskList.getElementsByTagName("li");
+    let checkers = myTaskList.getElementsByTagName("input");
+    let idNum = element.id.slice(7);
+    document.getElementById(`myItem${idNum}`).setAttribute("class","makegreen");
+
+
+}
+
 function addToList(){
-    theList.push(`<li>${myUserInput.value}</li>`);
+    theList.push(`<li>${myUserInput.value}</li><input type="checkbox">`);
     myTaskList.innerHTML = "";
     for (let i = 0; i < theList.length; i ++){
         myTaskList.innerHTML += theList[i];
     }
     let listItems = myTaskList.getElementsByTagName("li");
+    let checkers = myTaskList.getElementsByTagName("input");
     for (let x = 0; x < listItems.length; x++){
         listItems[x].setAttribute(`id`, `myItem${x}`);
+        checkers[x].setAttribute(`id`, `myCheck${x}`);
+        checkers[x].setAttribute("onclick", "toMakeGreen(this)");
     }
     console.log(myTaskList.innerHTML);
     myUserInput.value = "";
